04264bf03be6bd3070e19cf0fcfa07c96edd1ab4
Kiran Sathyanarayana <kiran.dulcet@gmail.com>
2024-10-06 16:14:19 +0530

    my first commit

---
+++ src/app/blog/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return(
+        <>
+            <h1 className="text-purple-500">About</h1>
+            <p>Save and see your changes instantly.</p>
+        </>
+    );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return(
+        <>
+            <h1>Blog</h1>
+            <p>Welcome to blog page</p>
+        </>
+    );
+}
